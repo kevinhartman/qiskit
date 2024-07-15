e74e682edb84fd8bc8040c57acd1760db9ef505e
Your Name <arbel@il.ibm.com>
2024-07-15 15:54:54 +0300

    Reverting a documentation change

--- crates/circuit/src/dag_circuit.rs
+++ crates/circuit/src/dag_circuit.rs
@@ -1808,7 +1808,7 @@ def _format(operand):
     /// Return the total number of qubits + clbits used by the circuit.
     /// This function formerly returned the number of qubits by the calculation
     /// return len(self._wires) - self.num_clbits()
-    /// but was changed by issue #2564 to return number of qubits + 
+    /// but was changed by issue #2564 to return number of qubits + clbits
     /// with the new function DAGCircuit.num_qubits replacing the former
     /// semantic of DAGCircuit.width().
     fn width(&self) -> usize {
